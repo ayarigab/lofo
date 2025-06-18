ca8e342a3950e66cdb139012c417ea55884147fb
Naaba Technologies LLC <jessybright66@gmail.com>
2025-06-18 23:56:05 +0000

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,29 @@
 <x-layouts.app :title="__('Dashboard')">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+    <div class="flex h-full w-full flex-1 flex-col gap-6 rounded-xl">
+        <div class="grid auto-rows-min gap-6 md:grid-cols-4">
+            <div class="relative aspect-video overflow-hidden rounded-xl bg-white shadow-lg p-5 flex-col">
+                <div class="relative  flex w-full items-center justify-center">
+                    <span class="text-lg font-semibold text-gray-800 dark:text-gray-200">Welcome to the Dashboard</span>
+                </div>
+                <div class="relative flex h-full items-center justify-center">
+                    <span class="text-lg font-semibold text-gray-800 dark:text-gray-200">Welcome to the Dashboard</span>
+                </div>
             </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <div class="relative aspect-video overflow-hidden rounded-xl bg-white shadow-lg p-5 flex-col">
+                <div class="relative flex w-full items-center justify-center">
+                    <span class="text-lg font-semibold text-gray-800 dark:text-gray-200">Welcome to the Dashboard</span>
+                </div>
+                <div class="relative flex h-full items-center justify-center">
+                    <span class="text-lg font-semibold text-gray-800 dark:text-gray-200">Welcome to the Dashboard</span>
+                </div>
+            </div>
+            <div class="relative aspect-video overflow-hidden rounded-xl bg-white shadow-lg p-5 flex-col">
+                <div class="relative flex w-full items-center justify-center">
+                    <span class="text-lg font-semibold text-gray-800 dark:text-gray-200">Welcome to the Dashboard</span>
+                </div>
+                <div class="relative flex h-full items-center justify-center">
+                    <span class="text-lg font-semibold text-gray-800 dark:text-gray-200">Welcome to the Dashboard</span>
+                </div>
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
